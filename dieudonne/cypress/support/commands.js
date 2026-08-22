a226c84bcf29956b51848a6fa3a539e2b9cbe987
Dieudonne Lusolo <mulongeshadieudonne17@gmail.com>
2026-08-22 11:31:13 +0100

    first test provide feedback  Cypress

--- dieudonne/cypress/support/commands.js
+++ dieudonne/cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("loginAsStudent", () => {
+  cy.visit("/");
+
+  // Nous allons ajouter ici les étapes réelles du login
+});
